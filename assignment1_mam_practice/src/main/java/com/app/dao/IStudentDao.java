7a86c4143e419660ce23326e804535ed0cba11ea
abhishek <abhishek.potbhare@viit.ac.in>
2021-09-18 10:50:23 +0530

    deltion of files

--- assignment1_mam_practice/src/main/java/com/app/dao/IStudentDao.java
+++
@@ -1,7 +0,0 @@
-package com.app.dao;
-
-import com.app.pojos.Student;
-
-public interface IStudentDao {
-	String admitStudent(Student student, String courseTitle);	
-}
--- assignment1_mam_practice/src/main/java/com/app/service/IStudentService.java
+++
@@ -1,7 +0,0 @@
-package com.app.service;
-
-import com.app.pojos.Student;
-
-public interface IStudentService {
-  String studentAdmiision(Student student,String courseTitle);
-}
