492340de652285fa41d740da01f0583db4d003a3
abhishek <abhishek.potbhare@viit.ac.in>
2021-09-18 10:47:52 +0530

    deletion of files

---
+++ assignment1_mam_practice/src/main/java/com/app/service/StudentServiceImpl.java
@@ -0,0 +1,13 @@
+package com.app.service;
+
+import com.app.pojos.Student;
+
+public class StudentServiceImpl implements IStudentService {
+
+	@Override
+	public String studentAdmiision(Student student, String courseTitle) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ assignment1_mam_practice/src/test/java/com/app/service/TestCourseService.java
@@ -0,0 +1,16 @@
+package com.app.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCourseService {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
